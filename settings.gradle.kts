ade2b709c7ecc78e7a958a129b66a6373d5e28c2
pvonhaden <pvonhaden@wisc.edu>
2023-10-03 20:01:39 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lab04-Milestone2"
+include(":app")
+ 
